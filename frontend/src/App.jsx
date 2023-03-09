cb455a42666a690ea28909873d509f510a395f9e
SlankehWeb <101549379+SlankehWeb@users.noreply.github.com>
2023-03-09 09:13:36 +0100

    added navbar and header with a footer

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './Components/Partials/Header'
+import Footer from './Components/Partials/Footer'
+
+
+
+const App = () => {
+    return (
+      <div className="App">
+        <Header />
+        <Footer />
+      </div>
+    );
+  }
+  
+  export default App;
---
+++ frontend/src/Components/Partials/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBarPrimary from './NavBarPrimary'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>INSERT LOGO</h1>
+            <NavBarPrimary />
+        </header>
+    )
+}
+
+export default Header;
