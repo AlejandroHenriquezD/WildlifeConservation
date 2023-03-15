9d314ba7c99d7f5dfefc04dd4b3cc651a14cb9fb
Erla-B <111748576+Erla-B@users.noreply.github.com>
2023-03-15 13:42:00 +0000

    Working on it

--- frontend/src/Components/Pages/Locations/Locations.jsx
+++ frontend/src/Components/Pages/Locations/Locations.jsx
@@ -2,6 +2,7 @@
 import { Outlet } from "react-router-dom"
 import LocationList from "./Partials/LocationList"
 import LLP from "./Partials/Locations-LandingPage"
+import "./Partials/Locations-LandingPage.scss"
 
 const Locations = () => {
     return (
--- frontend/src/Components/Pages/Locations/Partials/Locations-LandingPage.jsx
+++ frontend/src/Components/Pages/Locations/Partials/Locations-LandingPage.jsx
@@ -1,23 +1,31 @@
+import "./Locations-LandingPage.scss";
+import { BsArrowDownCircle } from "react-icons/bs";
+
 const LLP = () => {
-    return (
-      <>
-      <div>
-      <h2>WILDLIFE CONSERVATION</h2>
-      <h1>LOCATIONS</h1>
-      <h3>Here is a list of locations that are working <br/>toward protecting our wildlife</h3>
+  return (
+    <>
+      <div className="locations-background">
+        <div className="locations-image-text">
+          <h2 className="locations-h2">WILDLIFE CONSERVATION</h2>
+          <h1 className="locations-h1">LOCATIONS</h1>
+          <h3 className="locations-h3">
+            Here is a list of locations that are working <br />
+            toward protecting our wildlife
+          </h3>
+        </div>
       </div>
-      <button>make arrow down button</button>
+      <BsArrowDownCircle className="arrow-down" />
       <div>
-      <form action="#">
-      <input type="text" placeholder="Search.." />
-      <select name="animal" id="Animal" >
-        <option value="filter by animal">filter by animal</option>
-        <option value="animal2">animal2</option>
-      </select>
-</form>
+        <form action="#">
+          <input type="text" placeholder="Search.." />
+          <select name="animal" id="Animal">
+            <option value="filter by animal">filter by animal</option>
+            <option value="animal2">animal2</option>
+          </select>
+        </form>
       </div>
-      </>
-    )
-  }
-  
-  export default LLP
+    </>
+  );
+};
+
+export default LLP;
